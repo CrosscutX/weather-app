62709662668ddd710457f72f7e6f3e609206f7e3
Crosscut <crosscutcode@gmail.com>
2022-11-17 21:32:17 -0500

    Add basic error handeling

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 //I would not reveal this api key if it was important;
 const key = "6b97119aabe539419812d093ed6176e7";
 const inputBox = document.querySelector("input");
+const errorMsg = document.querySelector("header p");
 //object to hold all the information that I will display
 let info = {
   location: "New York",
@@ -14,43 +15,50 @@ let info = {
 getNewFranklin();
 
 async function getNewFranklin() {
-  const response = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=new%20franklin&appid=" +
-      key
-  );
+  try {
+    const response = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=new%20franklin&appid=" +
+        key
+    );
 
-  const data = await response.json();
+    const data = await response.json();
 
-  info.location = "New Franklin";
-  info.description = data.weather[0].description;
-  info.temperature = convertKelvin(data.main.temp);
-  info.wind = data.wind.speed + " mph";
-  info.humidity = data.main.humidity + "%";
+    info.location = "New Franklin";
+    info.description = data.weather[0].description;
+    info.temperature = convertKelvin(data.main.temp);
+    info.wind = data.wind.speed + " mph";
+    info.humidity = data.main.humidity + "%";
 
-  displayWeather();
+    displayWeather();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getWeather() {
-  console.log(info);
-  const response = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      inputBox.value +
-      "&appid=" +
-      key
-  );
-  const data = await response.json();
+  try {
+    const response = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        inputBox.value +
+        "&appid=" +
+        key
+    );
+    const data = await response.json();
 
-  info.location = inputBox.value;
-  info.description = data.weather[0].description;
-  info.temperature = convertKelvin(data.main.temp);
-  info.wind = data.wind.speed + " mph";
-  info.humidity = data.main.humidity + "%";
-  console.log(info);
-  displayWeather();
+    info.location = inputBox.value;
+    info.description = data.weather[0].description;
+    info.temperature = convertKelvin(data.main.temp);
+    info.wind = data.wind.speed + " mph";
+    info.humidity = data.main.humidity + "%";
+    errorMsg.style.visibility = "hidden";
+    displayWeather();
+  } catch (error) {
+    console.log(error);
+    errorMsg.style.visibility = "visible";
+  }
 }
 
 function displayWeather() {
-  console.log(info);
   const description = document.querySelector(".description");
   const location = document.querySelector(".location");
   const temperature = document.querySelector(".temperature");
